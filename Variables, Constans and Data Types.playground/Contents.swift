2a387d4aa9fd46539688275aaeb50d0a2d10c343
master <kammoren@icloud.com>
2017-09-07 14:41:56 +0200

    Dicee
    Playground

---
+++ Variables, Constans and Data Types.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myAge : Int = 23
+
+myAge = 24
+
+let myName : String = "Kamil"
+
+let myAgeInTenYears = myAge + 10
+
+let myFullName = myName + " Morskee"
+
+let myDetails = "\(myName), \(myAge)"
+
+let wholeNumbers : Int = 12
+let text : String = "abc"
+
+let booleans : Bool = true
+
+let floatingPointNumber : Float = 1.3
+
+let double : Double = 3.14159265359
+
+
+
+
+
