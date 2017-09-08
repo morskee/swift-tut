da200224342da64fd63b0b93b13f2b861624da41
master <kammoren@icloud.com>
2017-09-08 14:01:18 +0200

    Xylophone

---
+++ xylophone-master/Xylophone/ VIewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 27/01/2016.
+//  Copyright © 2016 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import AudioToolbox
+
+class ViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        if let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav") {
+            var mySound: SystemSoundID = 0
+            AudioServicesCreateSystemSoundID(soundURL as CFURL, &mySound)
+            // Play
+            AudioServicesPlaySystemSound(mySound);
+        }
+    }
+}
