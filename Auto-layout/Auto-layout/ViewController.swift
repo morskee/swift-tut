fee4192422c45947640f8a2eff965b85f7b05273
master <kammoren@icloud.com>
2017-10-02 13:47:24 +0200

    Auto layout

---
+++ Auto-layout/Auto-layout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Auto-layout
+//
+//  Created by Kamil Moreński on 02.10.2017.
+//  Copyright © 2017 Kamil Moreński. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
