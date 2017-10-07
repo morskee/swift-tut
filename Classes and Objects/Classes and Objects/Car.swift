1fd444dc6e24ca04d2386e72ab49d26dc34b42ab
master <kammoren@icloud.com>
2017-10-07 19:47:16 +0200

    Classes and Objects

---
+++ Classes and Objects/Classes and Objects/Car.swift
@@ -0,0 +1,31 @@
+//
+//  Car.swift
+//  Classes and Objects
+//
+//  Created by Kamil Moreński on 07.10.2017.
+//  Copyright © 2017 Kamil Moreński. All rights reserved.
+//
+
+import Foundation
+
+enum CarType {
+    case Sedan
+    case Coupe
+    case Hatchback
+}
+
+class Car {
+    
+    var colour = "Black"
+    var numberOfSeats = 5
+    var typeOfCar : CarType = .Coupe
+    
+    init() {
+        
+    }
+    
+    convenience init (customerChosenColour : String) {
+        self.init()
+        colour = customerChosenColour
+    }
+}
---
+++ Classes and Objects/Classes and Objects/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Classes and Objects
+//
+//  Created by Kamil Moreński on 07.10.2017.
+//  Copyright © 2017 Kamil Moreński. All rights reserved.
+//
+
+import Foundation
+
+let myCar = Car()
+
+let someRichGuysCar = Car(customerChosenColour: "Gold")
+
+print(myCar.colour)
+print(myCar.numberOfSeats)
+print(myCar.typeOfCar)
+
+print(someRichGuysCar.colour)
+print(someRichGuysCar.numberOfSeats)
+print(someRichGuysCar.typeOfCar)
