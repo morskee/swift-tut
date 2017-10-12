c660c61f8e8f6a25b14a4f35bb256aecf4b150ce
master <kammoren@icloud.com>
2017-10-12 16:12:30 +0200

    Clima
    working pods!!!!!!!!!!

--- Clima-master/Clima/WeatherViewController.swift
+++ Clima-master/Clima/WeatherViewController.swift
@@ -7,17 +7,17 @@
 //
 
 import UIKit
+import CoreLocation
 
-
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
     let APP_ID = "e72ca729af228beabd5d20e3b7749713"
-    
+    // 9311df0206b401d7a3d923373122d819
 
     //TODO: Declare instance variables here
-    
+    let locationManager = CLLocationManager()
 
     
     //Pre-linked IBOutlets
@@ -31,7 +31,9 @@ class WeatherViewController: UIViewController {
         
         
         //TODO:Set up the location manager here.
-    
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
         
         
     }
